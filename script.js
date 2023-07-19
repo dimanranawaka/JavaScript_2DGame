3ee7fc1d59fdd44b3925b8ba335d430164fd5178
dimanranawaka <dimanranawaka@gmail.com>
2023-07-19 21:31:55 +0530

    Creating classes like Java Using Syntactical Sugar

--- script.js
+++ script.js
@@ -13,5 +13,43 @@ window.addEventListener('load',function (){
     canvas.width = 1500;
     canvas.height = 500;
 
+    /* JavaScript is a prototype based Object-Oriented,which means it doesn't have classes , It has prototypes. We used
+    Syntactical Sugar to mimics classes Like Java Classes */
 
-})
+    class InputHandler {
+        // This class will keep track of specified user inputs
+    }
+
+    class Projectile {
+        // This class will handle player lasers
+    }
+
+    class Particle {
+        // This class will deal with falling screws, corks and bolts that from damage enemies
+    }
+
+    class Player {
+        // This class will control the main character
+    }
+
+    class Enemy {
+        // This class will be the main blueprint handling many different enemy types
+    }
+
+    class Layer {
+        // This class will handle individual background layers
+    }
+
+    class Background {
+        // This Class will pull all layers objects together to animate the entire game
+    }
+
+    class UI {
+        // This class will draw score , timer and other information that needs to be displayed for the user
+    }
+
+    class Game {
+        // This class will be the Brain of the entire Project
+    }
+});
+
