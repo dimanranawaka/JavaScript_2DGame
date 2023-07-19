ea64410f32790ca7a97c35f19c3267aba82a4a12
dimanranawaka <dimanranawaka@gmail.com>
2023-07-19 20:55:26 +0530

    canvas setting up

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 
 // Load Event fires when the whole page has been, Loaded , Including all dependent resources such as stylesheet and images
 window.addEventListener('load',function (){
+
     // canvas setup
+    const canvas = document.getElementById('canvas1');
+
+    const ctx = canvas.getContext();
+
+     /*a built-in object that contains all methods and properties that allows to draw and animate colors,shapes
+     and other graphics on HTML Canvas*/
+
+
+
+
 })
