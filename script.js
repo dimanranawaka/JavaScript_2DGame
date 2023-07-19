ea8a85d6821f262c1d4c08d175dff441612be021
dimanranawaka <dimanranawaka@gmail.com>
2023-07-19 20:41:15 +0530

    script.js file setting up

---
+++ script.js
@@ -0,0 +1,5 @@
+
+// Load Event fires when the whole page has been, Loaded , Including all dependent resources such as stylesheet and images
+window.addEventListener('load',function (){
+    // canvas setup
+})
